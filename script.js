47f576a1f2e98068af32f74f64d7aecbf2ab5452
Cameron Chardukian <camchardukian@gmail.com>
2022-02-13 17:55:37 -0600

    feature/axes

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  console.log("loaded whoaaaa");
-  const response = await fetch(
-    "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
-  );
-  if (!response.ok) {
-    const errorMessage = `An error has occured: ${response.status}`;
-    throw new Error(errorMessage);
-  }
-  const { data } = await response.json();
-  console.log("data", data);
+    const response = await fetch(
+        "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+    );
+    if (!response.ok) {
+        const errorMessage = `An error has occured: ${response.status}`;
+        throw new Error(errorMessage);
+    }
+    const { data } = await response.json();
+    console.log("data", data);
 
-  const width = 800;
-  const height = 400;
+    const width = 800;
+    const height = 400;
+    const padding = 60;
+    const scaleX = d3.scaleLinear()
+        .domain([d3.min(data, (d) => {
+            return parseInt(d[0].substring(0, 4))
+        }), d3.max(data, (d) => {
+            return parseInt(d[0].substring(0, 4))
+        })])
+        .range([padding, width - padding])
+    const scaleY = d3.scaleLinear()
+        .domain([0, d3.max(data, (d) => d[1])])
+        .range([height - padding, padding])
 
-  const svg = d3
-    .select("body")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    // @ TODO -- Scale this data later.
-    .attr("x", (_, i) => i * 2.5)
-    .attr("y", (d) => height - 0.02 * d[1])
-    .attr("width", 2)
-    .attr("height", (d) => 0.02 * d[1])
-    .attr("class", "bar")
-    .attr("data-date", (d) => d[0])
-    .attr("data-gdp", (d) => d[1]);
+    const svg = d3
+        .select("body")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+    // AXES
+    const xAxis = d3.axisBottom(scaleX)
+    const yAxis = d3.axisLeft(scaleY)
+
+    svg.append("g").attr("id", "x-axis").attr("transform", `translate(0, ${height - padding})`).call(xAxis)
+    svg.append("g").attr("id", "y-axis").attr("transform", `translate(${padding}, 0)`).call(yAxis)
+        // RECT
+        .selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", (_, i) => (width - (padding * 2)) / data.length * i)
+        .attr("y", (d) => scaleY(d[1]))
+        .attr("width", (width - (padding * 2)) / data.length * 0.75)
+        .attr("height", (d) => height - padding - scaleY(d[1]))
+        .attr("class", "bar")
+        .attr("data-date", (d) => d[0])
+        .attr("data-gdp", (d) => d[1])
 });
