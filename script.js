cce313c154669368578911bf52d002820d5c2ae1
Cameron Chardukian <camchardukian@gmail.com>
2022-02-13 00:45:20 -0600

    feature/fetch-data

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-console.log("Confirm script.js is loaded into index.html");
-console.log("confirm D3 CDN is working and D3 methods are available", d3);
+document.addEventListener("DOMContentLoaded", async () => {
+  console.log("loaded whoaaaa");
+  const response = await fetch(
+    "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+  );
+  if (!response.ok) {
+    const errorMessage = `An error has occured: ${response.status}`;
+    throw new Error(errorMessage);
+  }
+  const { data } = await response.json();
+  console.log("data", data);
+});
